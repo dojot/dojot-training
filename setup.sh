5fd092c07f59af09fcb4f0e6a2250417924bb197
Rafael Augusto Scaraficci <raugusto@cpqd.com.br>
2019-01-17 10:02:37 -0200

    Changed the flowbroker's registry to be optional in the setup script
    Now, when the flowbroker's docker registry is not set is created
    a localhost registry.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function print_help() {
-  echo "usage: $0 -m <MODE> -f <REGISTRY_IP>:<REGISTRY_PORT> [OPTIONS]"
+  echo "usage: $0 -m <MODE> [OPTIONS]"
   echo ""
   echo "  the mandatory parameters are:"
-  echo "    -m | --mode [mqtt | http] : select the kind of device will be used"
-  echo "    -f | --flowbroker-node-registry <REGISTRY_IP>:<REGISTRY_PORT> : inform which is the registry used by the flowbroker"
+  echo "    -m | --mode [mqtt | http] : enables the chosen iot-agent. This must be chosen according to the hands-on tasks."
   echo "  where OPTIONS could be:"
-  echo "    -s | --shared-flowbroker-node-registry : if the docker register used by the flowbroker is shared"
-  echo "    -i | --image-docker-registry <REGISTRY_IP>:<REGISTRY_PORT> : Docker register with dojot's images. Defaults to docker hub."
+  echo "    -f | --flowbroker-node-registry <REGISTRY_IP>:<REGISTRY_PORT> : informs which is the registry used by the flowbroker. If not set, a localhost registry will be used."
+  echo "    -s | --shared-flowbroker-node-registry : Must be set if the docker registry used by the flowbroker is shared by multiple users."
+  echo "    -i | --image-docker-registry <REGISTRY_IP>:<REGISTRY_PORT> : Docker registry with dojot's images. Defaults is to use docker hub."
 }
 
 
@@ -50,7 +50,6 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
-
 # help requested
 if [ ${PRINT_HELP} ] ; then
   print_help
@@ -59,23 +58,34 @@ fi
 
 
 # check mandatory parameters
-if [ -z "${MODE}" ] || [ -z "${FLOWBROKER_NODE_REGISTRY}" ] ; then
+if [ -z "${MODE}" ]; then
   echo "missing mandatory parameter"
   print_help
   exit 1
 fi
 
+## TODO: It would be nice to have some kind of input files
+## with permanent tags, and generate the output files from them.
+## In the current implementation after run the script for the first time, 
+## you cannot set the repositories anymore. 
 
 # build the docker-compose file
-if [ "${MODE}" = "mqtt" ] ; then
-	cat docker-compose/dojot-base.yml docker-compose/dojot-mqtt.yml docker-compose/network.yml > docker-compose/docker-compose.yml
+DOCKER_COMPOSE_FILE=docker-compose/docker-compose.yml
+cat docker-compose/dojot-base.yml > ${DOCKER_COMPOSE_FILE}
+if [ "${MODE}" = "mqtt" ]; then
+  cat docker-compose/dojot-mqtt.yml >> ${DOCKER_COMPOSE_FILE}
 elif [ "${MODE}" = "http" ] ; then
-	cat docker-compose/dojot-base.yml docker-compose/dojot-http.yml docker-compose/network.yml > docker-compose/docker-compose.yml
+  cat docker-compose/dojot-http.yml >> ${DOCKER_COMPOSE_FILE}
 else
   echo "invalid mode: " ${MODE}
+  rm ${DOCKER_COMPOSE_FILE}
   exit 1
 fi
-
+if [ -z "${FLOWBROKER_NODE_REGISTRY}" ]; then
+  cat docker-compose/dojot-registry.yml >> ${DOCKER_COMPOSE_FILE}
+  FLOWBROKER_NODE_REGISTRY="localhost:5009"
+fi
+cat docker-compose/network.yml >> ${DOCKER_COMPOSE_FILE}
 
 LOOKUP_DIRS="samples/ tasks/ docker-compose/"
 
@@ -84,14 +94,15 @@ if [ -n "${IMAGE_DOCKER_REGISTRY}" ] ; then
   IMAGE_DOCKER_REGISTRY=${IMAGE_DOCKER_REGISTRY}"\/"
 fi
 LOOKUP_TAG="<image-docker-registry>"
-grep -ril "${LOOKUP_TAG}" ${LOOKUP_DIRS} | xargs sed -i "s/\(.*\)${LOOKUP_TAG}\(.*\)/\1${IMAGE_DOCKER_REGISTRY}\2/"
+grep -ril "${LOOKUP_TAG}" ${LOOKUP_DIRS} | xargs -r sed -i "s/\(.*\)${LOOKUP_TAG}\(.*\)/\1${IMAGE_DOCKER_REGISTRY}\2/"
+
 
 # replace the flowbroker node docker registry
 if [ -n "${FLOWBROKER_NODE_REGISTRY}" ] ; then
   FLOWBROKER_NODE_REGISTRY=${FLOWBROKER_NODE_REGISTRY}"\/"
 fi
 LOOKUP_TAG="<flowbroker-node-docker-registry>"
-grep -ril "${LOOKUP_TAG}" ${LOOKUP_DIRS} | xargs sed -i "s/\(.*\)${LOOKUP_TAG}\(.*\)/\1${FLOWBROKER_NODE_REGISTRY}\2/"
+grep -ril "${LOOKUP_TAG}" ${LOOKUP_DIRS} | xargs -r sed -i "s/\(.*\)${LOOKUP_TAG}\(.*\)/\1${FLOWBROKER_NODE_REGISTRY}\2/"
 
 
 # deal with shared flowbroker node docker registry
@@ -99,4 +110,4 @@ RANDOM_TAG="<unique-id>"
 if [ ${SHARED_FLOWBROKER_NODE_REGISTRY} ]; then
   UNIQUE_ID=$(shuf -i 0-9999 -n 1)
 fi
-grep -ril "${RANDOM_TAG}" ${LOOKUP_DIRS} | xargs sed -i "s/\(.*\)${RANDOM_TAG}\(.*\)/\1${UNIQUE_ID}\2/"
+grep -ril "${RANDOM_TAG}" ${LOOKUP_DIRS} | xargs -r sed -i "s/\(.*\)${RANDOM_TAG}\(.*\)/\1${UNIQUE_ID}\2/"
